d32c08461c1b32ebc5efcb4d136ef98a66df5ef2
Radoslav Nikolov <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-08-14 10:20:38 +0300

    Add files via upload

---
+++ Programing Fundamentals/02.DataTypesAndVariables-MoreExercis/06.BalancedBrackets/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _06.BalancedBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberLines = int.Parse(Console.ReadLine());
+            int coutOpen = 0;
+            int coutCloes = 0;
+
+            for (int i = 0; i < numberLines; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "(")
+                {
+                    coutOpen++;
+                }
+                else if(input==")" && coutOpen==1)
+                {
+                    coutOpen = 0;
+                    coutCloes++;
+                }
+
+                else if (input == ")" && coutOpen == 0)
+                {
+                    coutOpen = 2;
+                }
+
+
+            }
+
+
+            if (coutOpen==0 && coutCloes>0)
+                Console.WriteLine("BALANCED");
+            else
+                Console.WriteLine("UNBALANCED");
+
+        }
+    }
+}
