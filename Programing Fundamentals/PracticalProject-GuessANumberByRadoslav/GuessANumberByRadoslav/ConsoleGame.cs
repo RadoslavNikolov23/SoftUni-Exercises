4c2c684ca42558fb6fdde8f68b066a5359e958e4
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-08-14 14:20:33 +0300

    Update the Console Game: Guess the Number

--- Programing Fundamentals/PracticalProject-GuessANumberByRadoslav/GuessANumberByRadoslav/ConsoleGame.cs
+++ Programing Fundamentals/PracticalProject-GuessANumberByRadoslav/GuessANumberByRadoslav/ConsoleGame.cs
@@ -7,11 +7,76 @@ namespace GuessANumberByRadoslav
         static void Main(string[] args)
         {
             Random randomNumber = new Random();
-            int numberFromComputer = randomNumber.Next(1, 101);
+            int countTries = 0;
+            int maxTries=0;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Let's play a Game: Guess the Number!");
+            Console.WriteLine();
 
             while (true)
             {
-                Console.WriteLine("Guess a number from 1 to 100: ");
+            Found:
+                maxTries=EnterTries(maxTries);
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Guess a number from 1 to 100, you have only {maxTries} tries");
+            
+                int numberFromComputer = randomNumber.Next(1, 101);
+
+                GameOn(numberFromComputer,maxTries,countTries);
+               
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Would you like to start a new Game - \"yes\" or \"no\"");
+                string answerForRestar = Console.ReadLine();
+                if (answerForRestar == "yes")
+                {
+                    countTries = 0;
+                    Console.WriteLine();
+                    goto Found;
+                }
+                else if (answerForRestar == "no")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Game Over! Bye");
+                    Console.WriteLine();
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Your input is Invalid!");
+                    Console.WriteLine("Game Over!");
+                    Console.WriteLine();
+                    break;
+                }
+            }
+        }
+
+        private static int EnterTries(int maxTries)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Enter how many tries would you like to have: ");
+            string inputTries = Console.ReadLine();
+
+            bool gameOn = int.TryParse(inputTries, out maxTries);
+
+            if (!gameOn)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid input! You will have 3 tries!");
+                maxTries = 3;
+            }
+            return maxTries;
+        }
+
+        private static void GameOn(int numberFromComputer, int maxTries, int countTries)
+        {
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine();
+                Console.Write("Your number is: ");
                 string inputByPlayer = Console.ReadLine();
 
                 bool isValid = int.TryParse(inputByPlayer, out int playerNumber);
@@ -20,24 +85,52 @@ namespace GuessANumberByRadoslav
                 {
                     if (playerNumber == numberFromComputer)
                     {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
                         Console.WriteLine("You guessed it!");
+                        Console.WriteLine();
                         break;
                     }
-                    else if(playerNumber>numberFromComputer)
+                    else if (playerNumber > numberFromComputer)
                     {
-                        Console.WriteLine("Your Number is too Hight!");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Your Number is too Hight! Try again.");
+                        countTries++;
+                        if (countTries >= maxTries)
+                        {
+                            break;
+                        }
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"You have {maxTries - countTries} tries left");
                     }
                     else
                     {
-                        Console.WriteLine("Your Number is too Low!");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Your Number is too Low! Try again.");
+                        countTries++;
+                        if (countTries >= maxTries)
+                        {
+                            break;
+                        }
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"You have {maxTries - countTries} tries left");
+                        Console.WriteLine();
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Your input is Invalid!");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Your input is Invalid! Try again.");
+                    countTries++;
+                    if (countTries >= maxTries)
+                    {
+                        break;
+
+                    }
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"You have {maxTries - countTries} tries left");
+                    Console.WriteLine();
                 }
             }
-
         }
     }
 }
