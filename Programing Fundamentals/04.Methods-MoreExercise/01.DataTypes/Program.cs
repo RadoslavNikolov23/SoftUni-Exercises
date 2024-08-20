66686c4e3d53619701b5c98411915ab21acb1a26
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-08-20 13:13:44 +0300

    Adding some extra exercise

---
+++ Programing Fundamentals/04.Methods-MoreExercise/01.DataTypes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _01.DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input=="int")
+            {
+                int numberInteger = int.Parse(Console.ReadLine());
+                GetResult(numberInteger);
+            }
+            else if(input =="float" || input =="real" || input=="double" || input == "decimal")
+            {
+                double numberDouble = double.Parse(Console.ReadLine());
+                GetResult(numberDouble);
+            }
+            else if (input == "string")
+            {
+                string stringInput = Console.ReadLine();
+                GetResult(stringInput);
+            }
+
+        }
+
+        private static void GetResult(int numberInteger)
+        {
+            int outPut = numberInteger * 2;
+            Console.WriteLine(outPut);
+        }
+
+        private static void GetResult(double numberDouble)
+        {
+            double outPut = numberDouble * 1.5;
+            Console.WriteLine($"{outPut:f2}");
+        }
+        private static void GetResult(string stringInput)
+        {
+            string output = $"${stringInput}$";
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Programing Fundamentals/04.Methods-MoreExercise/02.CenterPoint/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _02.CenterPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double radius = FindCPRadius(x1, y1);
+            double radius2 = FindCPRadius(x2, y2);
+
+            if (radius > radius2)
+            {
+                Console.WriteLine($"({x2}, {y2})");
+            }
+            else
+            {
+                Console.WriteLine($"({x1}, {y1})");
+
+            }
+
+
+        }
+
+        private static double FindCPRadius(double x, double y)
+        {
+
+            double radious = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            return radious;
+        }
+    }
+}
