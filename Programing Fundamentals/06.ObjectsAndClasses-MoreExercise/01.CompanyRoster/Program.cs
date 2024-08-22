be8900ba21797feb602bbe1faadb5954ce55ea41
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-08-22 15:37:11 +0300

    Uploading some extra exercise from the Obeject class

---
+++ Programing Fundamentals/06.ObjectsAndClasses-MoreExercise/01.CompanyRoster/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Numerics;
+
+namespace _01.CompanyRoster
+{
+  class Employee
+    {
+        public Employee(string name, double salary, string department)
+        {
+            Name = name;
+            Salary = salary;
+            Department = department;
+        }
+
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string Department { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberLine = int.Parse(Console.ReadLine());
+            List<Employee> listOfEmploeys = new List<Employee>();
+
+            for (int i = 0; i < numberLine; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string name = input[0];
+                double salary = double.Parse(input[1]);
+                string deparment = input[2];
+
+                listOfEmploeys.Add(new Employee(name, salary, deparment));
+
+            }
+
+
+            string highestPaidDeparment = string.Empty;
+            double maxSalary = int.MinValue;
+
+
+            var avarageSalary = listOfEmploeys.GroupBy(em => em.Department)
+                                                .Select(x => new { Department = x.Key, 
+                                                                   Avg = x.Average(a => a.Salary)});
+
+            foreach (var employee in avarageSalary)
+            {
+                if (employee.Avg > maxSalary)
+                {
+                    highestPaidDeparment = employee.Department;
+                    maxSalary = employee.Avg;
+                }
+
+            }
+
+            List <Employee> highestPaid = new List<Employee>();
+
+            foreach (var emploee in listOfEmploeys)
+            {
+                if (emploee.Department == highestPaidDeparment)
+                {
+                    highestPaid.Add(emploee);
+                }
+
+            }
+            highestPaid = highestPaid.OrderByDescending(x => x.Salary).ToList();
+
+            Console.WriteLine($"Highest Average Salary: {highestPaidDeparment}");
+            foreach (var empoleyy in highestPaid)
+            {
+                Console.WriteLine($"{empoleyy.Name} {empoleyy.Salary:f2}");
+
+            }
+
+        }
+    }
+}
---
+++ Programing Fundamentals/06.ObjectsAndClasses-MoreExercise/02.OldestFamilyMember/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Numerics;
+
+
+namespace _02.OldestFamilyMember
+{
+     class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+     class Family
+    {
+        public List<Person> listOfPeaple { get; set; } = new List<Person>();
+
+        public void AddPerson(string name, int age)
+        {
+            Person person = new Person(name, age);
+
+            listOfPeaple.Add(person);
+        }
+
+        public Person GetOldest()
+        {
+            return listOfPeaple.OrderByDescending(m => m.Age).First();
+        }
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                string[] array = Console.ReadLine().Split();
+                string name = array[0];
+                int age = int.Parse(array[1]);
+
+                family.AddPerson(name, age);
+            }
+
+            Person oldestPerson = family.GetOldest();
+
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+
+
+
+        }
+    }
+}
