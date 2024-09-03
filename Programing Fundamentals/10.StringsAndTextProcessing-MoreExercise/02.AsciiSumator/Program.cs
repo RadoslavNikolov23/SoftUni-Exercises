0ed0438ab658eccd45c7a3a80f8d5e98d17c4903
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-09-03 14:58:23 +0300

    Uploading the second task from the String and Text Processing exercise

---
+++ Programing Fundamentals/10.StringsAndTextProcessing-MoreExercise/02.AsciiSumator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02.AsciiSumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstCh = char.Parse(Console.ReadLine());
+            char secondCh = char.Parse(Console.ReadLine());
+            string thirdSt = Console.ReadLine();
+            int sumCh = 0;
+
+            for (int i = 0; i < thirdSt.Length; i++)
+            {
+                int tempChar = thirdSt[i];
+                if (tempChar>(int)firstCh && tempChar<(int)secondCh)
+                {
+                    sumCh += tempChar;
+                }
+
+            }
+            Console.WriteLine(sumCh);
+
+        }
+    }
+}
