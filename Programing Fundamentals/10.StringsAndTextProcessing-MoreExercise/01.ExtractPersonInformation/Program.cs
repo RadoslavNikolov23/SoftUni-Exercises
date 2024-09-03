068d1170e83c7e8d059ec74d2610093aa9865e02
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-09-03 14:33:52 +0300

    Uploading task from the exercise String and Text Processing

---
+++ Programing Fundamentals/10.StringsAndTextProcessing-MoreExercise/01.ExtractPersonInformation/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.ExtractPersonInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numberLines = int.Parse(Console.ReadLine());
+            Dictionary<string,string> nameAndAgeDT= new Dictionary<string,string>();
+
+            for (int i = 0; i < numberLines; i++)
+            {
+                string input = Console.ReadLine();
+                //string[] temp = input.Split(new[] { '@', '|', '#', '*'});
+                int index1=0;
+                int index2=0;
+                int index3=0;
+                int index4=0;
+
+                for (int k = 0; k < input.Length; k++)
+                {
+                    char temp = input[k];
+                    if (temp == '@')
+                    {
+                        index1 = k;
+                    }
+                    if (temp == '|')
+                    {
+                        index2 = k;
+                    }
+                    if (temp == '#')
+                    {
+                        index3 = k;
+                    }
+                    if (temp == '*')
+                    {
+                        index4 = k;
+                    }
+
+                }
+
+                string name=string.Empty;
+                string age=string.Empty;
+                name = input.Substring(index1+1,index2-index1-1);
+                age = input.Substring(index3+1, index4-index3-1);
+
+                nameAndAgeDT.Add(name, age);
+            }
+
+            foreach (var nameAge in nameAndAgeDT)
+            {
+                Console.WriteLine($"{nameAge.Key} is {nameAge.Value} years old.");
+
+            }
+        }
+    }
+}
