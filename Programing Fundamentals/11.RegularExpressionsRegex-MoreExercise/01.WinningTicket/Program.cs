3e4a0d1373293c77eca32b1ae24a1cc493e94504
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-09-05 11:40:06 +0300

    Uploading the first tast from the Regex MoreExervise lesson

---
+++ Programing Fundamentals/11.RegularExpressionsRegex-MoreExercise/01.WinningTicket/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace _01.WinningTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"(@{6,10}|#{6,10}|\${6,10}|\^{6,10})";
+
+
+            string[] input = Console.ReadLine().Split(',', StringSplitOptions.TrimEntries);
+
+            foreach (var ticket in input)
+            {
+
+                if (ticket.Length != 20)
+                {
+                    Console.WriteLine("invalid ticket");
+                    continue;
+                }
+
+                var leftSide = ticket.Substring(0, 10);
+                var rightSide = ticket.Substring(10);
+
+
+                var leftSideMatch = Regex.Match(leftSide, pattern).Value;
+                var rightSideMatch = Regex.Match(rightSide, pattern).Value;
+
+
+                if (string.IsNullOrEmpty(leftSideMatch) || string.IsNullOrEmpty(rightSideMatch) || leftSideMatch[0] != rightSideMatch[0])
+                {
+                    Console.WriteLine($"ticket \"{ticket}\" - no match");
+                    continue;
+                }
+                else
+                {
+                    int matchLength = Math.Min(leftSideMatch.Length, rightSideMatch.Length);
+                    var winningSymbol = leftSideMatch[0];
+
+                    if (matchLength >= 6 && matchLength <= 9)
+                    {
+                        Console.WriteLine($"ticket \"{ticket}\" - {matchLength}{winningSymbol}");
+                    }
+                    else if (matchLength > 9)
+                    {
+                        Console.WriteLine($"ticket \"{ticket}\" - {matchLength}{winningSymbol} Jackpot!");
+                    }
+                }
+
+
+            }
+
+        }
+    }
+}
