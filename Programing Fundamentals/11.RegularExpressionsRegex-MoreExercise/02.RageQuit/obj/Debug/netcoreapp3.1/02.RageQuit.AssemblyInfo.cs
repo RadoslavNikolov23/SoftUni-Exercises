752082e32d7dc04f3d21f9b7df67a6df9f97b86b
Rado <sitcom93@abv.bg>
2024-09-07 13:39:18 +0300

    Uploading the third task from the Regex MoreExercise lesson

--- Programing Fundamentals/11.RegularExpressionsRegex-MoreExercise/02.RageQuit/obj/Debug/netcoreapp3.1/02.RageQuit.AssemblyInfo.cs
+++ Programing Fundamentals/11.RegularExpressionsRegex-MoreExercise/02.RageQuit/obj/Debug/netcoreapp3.1/02.RageQuit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("02.RageQuit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27e111ffd3a163e00c96e8ac923e9a0fe8715add")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a5b04b9095c6631105b8d11069fa5e2db5abc48")]
 [assembly: System.Reflection.AssemblyProductAttribute("02.RageQuit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("02.RageQuit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
