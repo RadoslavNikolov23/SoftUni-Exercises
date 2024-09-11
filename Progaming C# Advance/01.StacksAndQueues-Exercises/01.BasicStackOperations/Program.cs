b99475eba2ed8ef3e80a8ccf32153b5e987b5b39
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-09-11 11:07:50 +0300

    Uploading the first task from the Stacks and Queue exercise lesson

---
+++ Progaming C# Advance/01.StacksAndQueues-Exercises/01.BasicStackOperations/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Numerics;
+
+namespace _01.BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int n = numbers[0], s = numbers[1], x = numbers[2];
+
+            int[] numbersToStack = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            Stack<int> stackNumbers = new Stack<int>();
+
+            for(int i = 0; i < n; i++)
+            {
+                stackNumbers.Push(numbersToStack[i]);
+            }
+
+            for(int i = 0; i < s; i++)
+            {
+                stackNumbers.Pop();
+            }
+
+            if (stackNumbers.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if(stackNumbers.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stackNumbers.Min());
+            }
+
+
+        }
+    }
+}
