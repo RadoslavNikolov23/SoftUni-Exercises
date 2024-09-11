baf90bf88561f7a46baeaa9d9bb172757c9d4098
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-09-11 16:17:58 +0300

    Uploading the sixth and seveth task from the Stack and Queue exercise lesson

---
+++ Progaming C# Advance/01.StacksAndQueues-Exercises/06.SongsQueue/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Numerics;
+
+namespace _06.SongsQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(", ");
+            Queue<string> songQU = new Queue<string>(array);
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split();
+
+                string firstCommand = command[0];
+
+                switch (firstCommand)
+                {
+                    case "Play":
+                        songQU.Dequeue();
+                        break;
+
+                    case "Add":
+                        string songName = command[1];
+                        for (int i = 2; i < command.Length; i++)
+                        {
+                            songName += $" {command[i]}";
+                        }
+                        if (!songQU.Contains(songName))
+                        {
+                            songQU.Enqueue(songName);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{songName} is already contained!");
+                        }
+                        break;
+
+                    case "Show":
+                        Console.WriteLine(string.Join(", ",songQU));
+                        break;
+                }
+
+
+
+                if (songQU.Count == 0)
+                {
+                    Console.WriteLine("No more songs!");
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Progaming C# Advance/01.StacksAndQueues-Exercises/07.TruckTour/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Numerics;
+
+namespace _07.TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPetrolPumps = int.Parse(Console.ReadLine());
+            Queue<int> amountQU = new Queue<int>();
+            Queue<int> distanceQU = new Queue<int>();
+
+            for (int i = 0; i < numberOfPetrolPumps; i++)
+            {
+                int[] pair = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+                int amountOfPetrol = pair[0];
+                int distanceOfKilom = pair[1];
+                amountQU.Enqueue(amountOfPetrol);
+                distanceQU.Enqueue(distanceOfKilom);
+            }
+
+            int indexStart = 0;
+            int currIndex = 0;
+            int fuel = amountQU.Peek();
+            int counterPetrolStation = 0;
+
+            while (numberOfPetrolPumps >= counterPetrolStation)
+            {
+                if (counterPetrolStation == 0)
+                {
+                    indexStart = currIndex;
+                }
+
+                if (fuel >= distanceQU.Peek())
+                {
+                  
+                    fuel -= distanceQU.Peek();
+                    counterPetrolStation++;
+                    amountQU.Enqueue(amountQU.Dequeue());
+                    distanceQU.Enqueue(distanceQU.Dequeue());
+                    fuel += amountQU.Peek();
+                }
+                else
+                {
+                    amountQU.Enqueue(amountQU.Dequeue());
+                    distanceQU.Enqueue(distanceQU.Dequeue());
+                    fuel = amountQU.Peek();
+                    counterPetrolStation = 0;
+                }
+                currIndex++;
+            }
+            Console.WriteLine(indexStart);
+
+        }
+    }
+
+}
