7a65af4d941055a9f2100e90da55d1eabf637738
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-09-12 16:12:52 +0300

    Uploading the ninth and tenth task from the Stacks and Queues Exercise lesson

---
+++ Progaming C# Advance/01.StacksAndQueues-Exercises/09.SimpleTextEditor/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Numerics;
+
+namespace _09.SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stackST = new Stack<string>();
+            StringBuilder sbTemp = new StringBuilder();
+
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] array = Console.ReadLine().Split();
+                string firstCommand = array[0];
+
+                if (firstCommand == "1")
+                {
+                    stackST.Push(sbTemp.ToString());
+                    sbTemp.Append(array[1]);
+
+                }
+                else if (firstCommand == "2")
+                {
+                    int counterToErase = int.Parse(array[1]);
+                    stackST.Push(sbTemp.ToString());
+                    sbTemp.Remove(sbTemp.Length - counterToErase, counterToErase);
+                   
+                }
+                else if (firstCommand == "3")
+                {
+                    int indexToReturn = int.Parse(array[1]) - 1;
+                    Console.WriteLine($"{sbTemp[indexToReturn]}");
+                  
+                }
+                else if (firstCommand == "4")
+                {
+                    sbTemp.Clear();
+                   sbTemp.Append( stackST.Pop());
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ Progaming C# Advance/01.StacksAndQueues-Exercises/10.Crossroads/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Numerics;
+
+namespace _10.Crossroads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> carsQU = new Queue<string>();
+            int greenLightSeconds = int.Parse(Console.ReadLine());
+            int freeWindowSeconds = int.Parse(Console.ReadLine());
+            string input;
+            int counterSafePassCars = 0;
+            bool crashHappen = false;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                if (input == "green")
+                {
+                    int greenCycle = greenLightSeconds;
+
+                    while (carsQU.Count > 0 && greenCycle > 0)
+                    {
+                        string carEnt = carsQU.Peek();
+
+
+                        if (greenCycle >= carEnt.Length)
+                        {
+                            greenCycle -= carEnt.Length;
+                            carsQU.Dequeue();
+                            counterSafePassCars++;
+                        }
+                        else
+                        {
+                            if (greenCycle + freeWindowSeconds >= carEnt.Length)
+                            {
+                                carsQU.Dequeue();
+                                greenCycle = 0;
+                                counterSafePassCars++;
+                            }
+                            else
+                            {
+                                crashHappen = true;
+                                Console.WriteLine("A crash happened!");
+                                Console.WriteLine($"{carEnt} was hit at {carEnt[greenCycle + freeWindowSeconds]}.");
+                                carsQU.Dequeue();
+                                break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    carsQU.Enqueue(input);
+                }
+
+                if (crashHappen)
+                {
+                    break;
+                }
+            }
+
+            if (!crashHappen)
+            {
+                Console.WriteLine("Everyone is safe.");
+                Console.WriteLine($"{counterSafePassCars} total cars passed the crossroads.");
+
+            }
+
+        }
+    }
+}
