6e2f4d1816283805b859005abe1efa861c333b8e
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-09-12 14:27:33 +0300

    Uploading the eight task from the Stacks and Queues Exercise lesson

---
+++ Progaming C# Advance/01.StacksAndQueues-Exercises/08.BalancedParentheses/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Numerics;
+
+namespace _08.BalancedParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textInput = Console.ReadLine();
+            Stack<char> stackCH = new Stack<char>();
+            bool isBalaned = false;
+
+            for(int i = 0; i < textInput.Length; i++)
+            {
+                char tempCH = textInput[i];
+
+                if (tempCH == '(')
+                {
+                    stackCH.Push(')');
+                }
+                else if (tempCH == '{')
+                {
+                    stackCH.Push('}');
+                } 
+                else if (tempCH == '[')
+                {
+                    stackCH.Push(']');
+                }
+                else
+                {
+                    if(stackCH.Count>0 && stackCH.Peek() == tempCH)
+                    {
+                        stackCH.Pop();
+                        isBalaned = true;
+                    }
+                    else
+                    {
+                        isBalaned = false;
+                        break;
+                    }
+
+                }
+            }
+            if (isBalaned)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+        }
+    }
+}
