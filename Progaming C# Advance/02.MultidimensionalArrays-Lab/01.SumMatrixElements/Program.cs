6337f9200c767fb6e6cb108c4a0f9346613b3302
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-09-13 10:46:53 +0300

    Iploading the first and second task from the Matrix array lesson

---
+++ Progaming C# Advance/02.MultidimensionalArrays-Lab/01.SumMatrixElements/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Numerics;
+
+namespace _01.SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayRowCol = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = arrayRowCol[0];
+            int cols= arrayRowCol[1];
+            int[,] matrix = GetMatrix(rows, cols);
+
+            int summAllEments=GetSumMatrixElements(matrix);
+           
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(summAllEments);
+        }
+
+        public static int GetSumMatrixElements(int[,] matrix)
+        {
+            int sumAllElements = 0;
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    sumAllElements += matrix[i, j];
+                }
+            }
+          return sumAllElements;
+        }
+
+        public static int[,] GetMatrix(int rows,int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] dataColon = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = dataColon[j];
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Progaming C# Advance/02.MultidimensionalArrays-Lab/02.SumMatrixColumns/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumMatrixColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dataRowCols = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int[,] matrix = InputMatrix(dataRowCols[0], dataRowCols[1]);
+
+            WriteSumAllCol(matrix);
+
+        }
+
+        private static void WriteSumAllCol(int[,] matrix)
+        {
+
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                int sumCol = 0;
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    sumCol += matrix[i, j];
+                }
+                    Console.WriteLine(sumCol);
+            }
+
+        }
+
+        private static int[,] InputMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] dataRows = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = dataRows[j];
+                }
+
+            }
+
+            return matrix;
+        }
+    }
+}
