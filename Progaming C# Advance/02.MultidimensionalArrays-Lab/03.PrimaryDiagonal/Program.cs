3604320c2e722653145a3dae5340fbe06214c7ab
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-09-13 12:03:26 +0300

    Uploading the third and forth task from the Matrix arrays lesson

---
+++ Progaming C# Advance/02.MultidimensionalArrays-Lab/03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowCol = int.Parse(Console.ReadLine());
+
+            int[,] matrix = InputMatrix(rowCol);
+
+            int primarDiagonlSum = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                primarDiagonlSum+= matrix[i, i];
+            }
+            Console.WriteLine(primarDiagonlSum);
+
+        }
+
+        private static int[,] InputMatrix(int rowCol)
+        {
+            int[,] matrix = new int[rowCol, rowCol];
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] dataCol = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for(int j=0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = dataCol[j];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ Progaming C# Advance/02.MultidimensionalArrays-Lab/04.SymbolInMatrix/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowCols = int.Parse(Console.ReadLine());
+
+            char[,] matrix = InputMatrix(rowCols, rowCols);
+
+            char sumbolToFind = char.Parse(Console.ReadLine());
+            bool isFound = false;
+            int[] coordOfSumb = new int[2];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == sumbolToFind)
+                    {
+                        coordOfSumb[0] = i;
+                        coordOfSumb[1] = j;
+                        isFound = true;
+                        break;
+                    }
+                }
+
+                if (isFound) break;
+            }
+
+            if (isFound)
+            {
+                Console.WriteLine($"({string.Join(", ",coordOfSumb)})");
+            }
+            else
+            {
+                Console.WriteLine($"{sumbolToFind} does not occur in the matrix");
+            }
+        }
+
+        public static char[,] InputMatrix(int rows, int cols)
+        {
+            char[,] matrix = new char[rows, cols];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                char[] charCol = Console.ReadLine().ToCharArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = charCol[j];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
