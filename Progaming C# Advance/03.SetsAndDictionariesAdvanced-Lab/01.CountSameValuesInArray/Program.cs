f7971b7a5313d304a6d783281dcfd5f1fbf97c3f
Rado <sitcom93@abv.bg>
2024-09-17 00:01:35 +0300

    Uploading the fist and second task from the Sets and Dictionaries Advanced lesson

---
+++ Progaming C# Advance/03.SetsAndDictionariesAdvanced-Lab/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01.CountSameValuesInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<double> numbersList=Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToList();
+
+            Dictionary<double,int>countDictionary=new Dictionary<double,int>();
+
+            foreach (double number in numbersList)
+            {
+                if (!countDictionary.ContainsKey(number))
+                {
+                    countDictionary.Add(number, 0);
+                }
+
+                countDictionary[number]++;
+            }
+
+            foreach (var pair in countDictionary)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value} times");
+            }
+        }
+    }
+}
---
+++ Progaming C# Advance/03.SetsAndDictionariesAdvanced-Lab/02.AverageStudentGrades/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02.AverageStudentGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStudents=int.Parse(Console.ReadLine());
+
+            Dictionary<string,List<decimal>> studensDictionary = new Dictionary<string,List<decimal>>();
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                string[]array=Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+                string nameStudent=array[0];
+                decimal studentGrade = decimal.Parse(array[1]);
+
+                if (!studensDictionary.ContainsKey(nameStudent))
+                {
+                    studensDictionary.Add(nameStudent, new List<decimal>());
+                }
+
+                studensDictionary[nameStudent].Add(studentGrade);
+            }
+
+            foreach (var student in studensDictionary)
+            {
+                decimal avarage =student.Value.Average();
+              
+                Console.WriteLine($"{student.Key} -> {string.Join(" ",student.Value.Select(x=>$"{x:f2}"))} (avg: {avarage:f2})");
+
+            }
+
+        }
+    }
+}
