da67781740f2b1cdd60ebc02500c3d087bdc3ce3
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-09-18 11:15:13 +0300

    Uploading the sixth and seventh task from the Sets and Dictionaries Advance lesson

---
+++ Progaming C# Advance/03.SetsAndDictionariesAdvanced-Lab/06.RecordUniqueNames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.RecordUniqueNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfname = int.Parse(Console.ReadLine());
+
+            HashSet<string> nameSet = new HashSet<string>();
+
+            for(int i = 0; i < numberOfname; i++)
+            {
+                nameSet.Add(Console.ReadLine());
+            }
+
+            foreach(var name in nameSet)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ Progaming C# Advance/03.SetsAndDictionariesAdvanced-Lab/07.ParkingLot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace _07.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> carParkingLot = new HashSet<string>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] array = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string direction = array[0].ToUpper();
+                string carNumber = array[1];
+
+                if (direction == "IN")
+                {
+                    carParkingLot.Add(carNumber);
+                }
+                else if (direction == "OUT")
+                {
+                    carParkingLot.Remove(carNumber);
+                }
+            }
+
+            if (carParkingLot.Count > 0)
+                Console.WriteLine(string.Join("\n", carParkingLot));
+            else
+                Console.WriteLine("Parking Lot is Empty");
+        }
+    }
+}
