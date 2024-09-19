727e11929ba07f1fb03a482bf320bce21bb4fae2
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-09-19 15:05:28 +0300

    Uploading the eight and ninth task from the Matrix Arrays Exercise lesson

---
+++ Progaming C# Advance/02.MultidimensionalArrays-Exercise/08.Bombs/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberRowsCol = int.Parse(Console.ReadLine());
+
+            int[,] squarMatrix = new int[numberRowsCol, numberRowsCol];
+
+            InputSquareMatrix(squarMatrix);
+
+            int[] bombs = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).ToArray();
+
+            FindBoms(squarMatrix, bombs);
+
+            int[] activeCellsAndSum = ActiveCells(squarMatrix);
+            int activeCellCount = activeCellsAndSum[0];
+            int sumCells = activeCellsAndSum[1];
+
+            PrintOutPut(squarMatrix, activeCellCount, sumCells);
+        }
+
+
+        private static void InputSquareMatrix(int[,] squarMatrix)
+        {
+            for (int i = 0; i < squarMatrix.GetLength(0); i++)
+            {
+                int[] data = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).ToArray();
+
+                for (int j = 0; j < squarMatrix.GetLength(1); j++)
+                {
+                    squarMatrix[i, j] = data[j];
+                }
+            }
+        }
+
+        private static void FindBoms(int[,] squarMatrix, int[] bombs)
+        {
+            for (int b = 0; b < bombs.Length; b++)
+            {
+                int bomRow = bombs[b];
+                int bomCol = bombs[++b];
+
+                for (int i = 0; i < squarMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < squarMatrix.GetLength(1); j++)
+                    {
+                        if (i == bomRow && j == bomCol)
+                        {
+                            if (squarMatrix[i, j] > 0)
+                            {
+
+                                BombExploed(squarMatrix, i, j);
+
+                                squarMatrix[i, j] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private static void BombExploed(int[,] squarMatrix, int i, int j)
+        {
+            int valueBomb = squarMatrix[i, j];
+
+            if (i - 1 >= 0 && j - 1 >= 0)
+            {
+                if (CheckValue(squarMatrix[i - 1, j - 1]))
+                    squarMatrix[i - 1, j - 1] -= valueBomb;
+            }
+            if (i - 1 >= 0)
+            {
+                if (CheckValue(squarMatrix[i - 1, j]))
+                    squarMatrix[i - 1, j] -= valueBomb;
+            }
+
+            if (i - 1 >= 0 && j + 1 < squarMatrix.GetLength(1))
+            {
+                if (CheckValue(squarMatrix[i - 1, j + 1]))
+                    squarMatrix[i - 1, j + 1] -= valueBomb;
+            }
+
+            if (j + 1 < squarMatrix.GetLength(1))
+            {
+                if (CheckValue(squarMatrix[i, j + 1]))
+                    squarMatrix[i, j + 1] -= valueBomb;
+            }
+
+            if (i + 1 < squarMatrix.GetLength(0) && j + 1 < squarMatrix.GetLength(1))
+            {
+                if (CheckValue(squarMatrix[i + 1, j + 1]))
+                    squarMatrix[i + 1, j + 1] -= valueBomb;
+            }
+
+            if (i + 1 < squarMatrix.GetLength(0))
+            {
+                if (CheckValue(squarMatrix[i + 1, j]))
+                    squarMatrix[i + 1, j] -= valueBomb;
+            }
+
+            if (i + 1 < squarMatrix.GetLength(0) && j - 1 >= 0)
+            {
+                if (CheckValue(squarMatrix[i + 1, j - 1]))
+                    squarMatrix[i + 1, j - 1] -= valueBomb;
+            }
+
+            if (j - 1 >= 0)
+            {
+                if (CheckValue(squarMatrix[i, j - 1]))
+                    squarMatrix[i, j - 1] -= valueBomb;
+            }
+
+
+        }
+
+        private static bool CheckValue(int value)
+        {
+            if (value > 0) return true;
+            else return false;
+        }
+
+        private static int[] ActiveCells(int[,] squarMatrix)
+        {
+
+            int counActiveCells = 0;
+            int sumCells = 0;
+            for (int i = 0; i < squarMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < squarMatrix.GetLength(1); j++)
+                {
+                    if (squarMatrix[i, j] > 0)
+                    {
+                        counActiveCells++;
+                        sumCells += (squarMatrix[i, j]);
+                    }
+                }
+            }
+
+            int[] array = new int[] { counActiveCells, sumCells };
+
+            return array;
+        }
+
+        private static void PrintOutPut(int[,] squarMatrix, int activeCellCount, int sumCells)
+        {
+            Console.WriteLine($"Alive cells: {activeCellCount}");
+            Console.WriteLine($"Sum: {sumCells}");
+
+            for (int i = 0; i < squarMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < squarMatrix.GetLength(1); j++)
+                {
+                    if (j > 0) Console.Write(" ");
+                    Console.Write(squarMatrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Progaming C# Advance/02.MultidimensionalArrays-Exercise/09.Miner/Program.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Linq;
+
+namespace _09.Miner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int squareNumber = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[squareNumber, squareNumber];
+            string[] directions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            InputMatrix(matrix);
+
+            int[] startCoodrinAndCoals = FindCordinAnaCoals(matrix);
+
+            StartJorney(matrix, directions, startCoodrinAndCoals);
+
+            Console.WriteLine();
+        }
+
+        public static void StartJorney(char[,] matrix, string[] directions, int[] startCoodrinAndCoals)
+        {
+            int startRow = startCoodrinAndCoals[0];
+            int startCol = startCoodrinAndCoals[1];
+            int coalsSum = startCoodrinAndCoals[2];
+            int countCollectCoals = 0;
+            bool gameOn = true;
+            bool foundAllCoal = false;
+            bool gameOver = false;
+
+            while (gameOn)
+            {
+                for(int i = 0; i < directions.Length; i++)
+                {
+                    switch (directions[i])
+                    {
+                        case "up":
+                            if(startRow-1>=0)
+                            {
+                                char curnSym = matrix[startRow - 1, startCol];
+                                    startRow = startRow - 1;
+
+                                if (curnSym == '*')
+                                {
+                                    continue;
+                                }
+                                else if (curnSym == 'c')
+                                {
+                                    countCollectCoals++;
+                                    matrix[startRow, startCol] = '*';
+                                    coalsSum--;
+
+                                    if (0 >= coalsSum)
+                                    {
+                                        gameOn = false;
+                                        foundAllCoal = true;
+                                    }
+                                }
+                                else if (curnSym == 'e')
+                                {
+                                    gameOn = false;
+                                    gameOver = true;
+                                }
+                            }
+                            break;
+
+                        case "right":
+                            if (startCol+1<matrix.GetLength(1))
+                            {
+                                char curnSym = matrix[startRow, startCol+1];
+                                startCol = startCol + 1;
+
+                                if (curnSym == '*')
+                                {
+                                    
+                                    continue;
+                                }
+                                else if (curnSym == 'c')
+                                {
+                                    countCollectCoals++;
+                                    matrix[startRow, startCol] = '*';
+                                    coalsSum--;
+
+                                    if (0 >= coalsSum)
+                                    {
+                                        gameOn = false;
+                                        foundAllCoal = true;
+                                    }
+                                }
+                                else if (curnSym == 'e')
+                                {
+                                    gameOn = false;
+                                    gameOver = true;
+                                }
+                            }
+                            break;
+
+                        case "left":
+                            if (startCol - 1 >=0)
+                            {
+                                char curnSym = matrix[startRow, startCol - 1];
+                                startCol = startCol - 1;
+
+                                if (curnSym == '*')
+                                {
+                                    continue;
+                                }
+                                else if (curnSym == 'c')
+                                {
+                                    countCollectCoals++;
+                                    matrix[startRow, startCol] = '*';
+                                    coalsSum--;
+
+                                    if (0 >= coalsSum)
+                                    {
+                                        gameOn = false;
+                                        foundAllCoal = true;
+                                    }
+                                }
+                                else if (curnSym == 'e')
+                                {
+                                    gameOn = false;
+                                    gameOver = true;
+                                }
+                            }
+                            break;
+
+                        case "down":
+                            if (startRow +1<matrix.GetLength(0))
+                            {
+                                char curnSym = matrix[startRow+1, startCol];
+                                startRow = startRow + 1;
+
+                                if (curnSym == '*')
+                                {
+                                    continue;
+                                }
+                                else if (curnSym == 'c')
+                                {
+                                    countCollectCoals++;
+                                    matrix[startRow, startCol] = '*';
+                                    coalsSum--;
+
+                                    if (0 >= coalsSum)
+                                    {
+                                        gameOn = false;
+                                        foundAllCoal = true;
+                                    }
+                                }
+                                else if (curnSym == 'e')
+                                {
+                                    gameOn = false;
+                                    gameOver = true;
+                                }
+                            }
+                            break;
+                    }
+
+                    if (!gameOn) break;
+                }
+
+                if (foundAllCoal)
+                {
+                    Console.WriteLine($"You collected all coals! ({startRow}, {startCol})");
+                    gameOn = false;
+                }
+                else if (gameOver)
+                {
+                    Console.WriteLine($"Game over! ({startRow}, {startCol})");
+                    gameOn = false;
+                }
+                else
+                {
+                    Console.WriteLine($"{coalsSum} coals left. ({startRow}, {startCol})");
+                    gameOn = false;
+                }
+
+            }
+
+
+
+        }
+
+
+        public static int[] FindCordinAnaCoals(char[,] matrix)
+        {
+            int[] startCoord = new int[3];
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == 's')
+                    {
+                        startCoord[0] = i;
+                        startCoord[1] = j;
+                    }
+                    else if (matrix[i, j] == 'c')
+                    {
+                        startCoord[2]++;
+                    }
+                }
+                
+            }
+
+            return startCoord;
+        }
+
+        public static void InputMatrix(char[,] matrix)
+        {
+            for(int i=0; i < matrix.GetLength(0); i++)
+            {
+                char[] data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse).ToArray();
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = data[j];
+                }
+
+            }
+
+        }
+    }
+}
