23bcc6a7fef332f11ad5aa7e5021554610b0523f
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-09-24 14:23:16 +0300

    Uploading the seventh task from the Sets and Dictionaries Advance Exercise lesson

--- Progaming C# Advance/03.SetsAndDictionariesAdvanced-Exercise/01.UniqueUsernames/obj/Debug/netcoreapp3.1/01.UniqueUsernames.AssemblyInfo.cs
+++ Progaming C# Advance/03.SetsAndDictionariesAdvanced-Exercise/01.UniqueUsernames/obj/Debug/netcoreapp3.1/01.UniqueUsernames.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01.UniqueUsernames")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1ed270f023ca19f4adfc375f80911f3ce32f6dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("01.UniqueUsernames")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01.UniqueUsernames")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Progaming C# Advance/03.SetsAndDictionariesAdvanced-Exercise/07.TheV-Logger/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.TheV_Logger
+{
+    class Vlog
+    {
+        public Vlog(HashSet<string> followers, int coutManyFollowing)
+        {
+            Followers = followers;
+            CoutManyFollowing = coutManyFollowing;
+        }
+
+        public HashSet<string> Followers { get; set; }
+        public int CoutManyFollowing { get; set; }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, Vlog> vloggersAndFollowers = new Dictionary<string, Vlog>();
+
+            while (input != "Statistics")
+            {
+                string[] array = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string command = array[1];
+
+                switch (command)
+                {
+                    case "joined":
+                        string joinedVloger = array[0];
+                        if (!vloggersAndFollowers.ContainsKey(joinedVloger))
+                        {
+                            vloggersAndFollowers.Add(joinedVloger, new Vlog(new HashSet<string>(), 0));
+                        }
+
+                        break;
+
+                    case "followed":
+                        string follower = array[0];
+                        string vlogger = array[2];
+
+                        if (vloggersAndFollowers.ContainsKey(vlogger) &&
+                            vloggersAndFollowers.ContainsKey(follower) &&
+                            follower != vlogger &&
+                            vloggersAndFollowers[vlogger].Followers.Add(follower))
+                        {
+                            vloggersAndFollowers[follower].CoutManyFollowing++;
+                        }
+
+                        break;
+                }
+
+                input = Console.ReadLine();
+
+            }
+            PrintOutput(vloggersAndFollowers);
+        }
+
+        private static void PrintOutput(Dictionary<string, Vlog> vloggersAndFollowers)
+        {
+            Console.WriteLine($"The V-Logger has a total of {vloggersAndFollowers.Keys.Count} vloggers in its logs.");
+
+            int count = 1;
+            foreach (var (vlogger, following) in vloggersAndFollowers.OrderByDescending(x => x.Value.Followers.Count())
+                .ThenBy(x => x.Value.CoutManyFollowing))
+            {
+                Console.WriteLine($"{count}. {vlogger} : {following.Followers.Count()} followers, {following.CoutManyFollowing} following");
+                if (count == 1)
+                {
+                    foreach (var follower in following.Followers.OrderBy(f => f))
+                    {
+                        Console.WriteLine($"*  {follower}");
+                    }
+                }
+
+                count++;
+            }
+        }
+    }
+}
