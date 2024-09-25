6ac3a8c27f83ad38d2601a850910cb6e0fe2d844
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-09-25 16:00:57 +0300

    Uploading the first task from the Fuctional Programming lesson

---
+++ Progaming C# Advance/05.FunctionalProgramming-Lab/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ",Console.ReadLine().Split(',',StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).Where(num=>num%2==0).OrderBy(num=>num)));
+
+
+            /*
+            List<int> numbersList = Console.ReadLine().Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+
+            List<int> evenNumbersList = numbersList.Where(num => num % 2 == 0).Select(num => num).ToList();
+
+            Console.WriteLine($"{string.Join(", ",evenNumbersList.OrderBy(num=>num))}");
+            */
+
+            // Alternative solution #2:
+            /*
+            List<int> numbersList = Console.ReadLine().Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+            List<int> secondEvenList  = (from num in numbersList
+                                         where num % 2 == 0
+                                         select num).ToList();
+            Console.WriteLine($"{string.Join(", ", secondEvenList.OrderBy(num => num))}");
+            */
+        }
+    }
+}
