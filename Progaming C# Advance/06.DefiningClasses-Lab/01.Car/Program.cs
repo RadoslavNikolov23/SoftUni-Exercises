2fc3d7b74d127300a409d83caff6c30a7f4f8bf4
Rado <sitcom93@abv.bg>
2024-09-30 17:42:01 +0300

    Uploading the first task from the Defining classes lesson

---
+++ Progaming C# Advance/06.DefiningClasses-Lab/01.Car/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car()
+            {
+                Make = "VW",
+                Model = "MK3",
+                Year = 1992
+            };
+
+            //car.Make = "VW";
+            //car.Model = "MK3";
+            //car.Year = 1992;
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+
+    }
+}
