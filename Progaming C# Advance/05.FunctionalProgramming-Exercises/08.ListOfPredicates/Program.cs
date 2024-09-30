ee030d661b17f1281c7a4d8495637d285f76b65f
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-09-30 11:43:28 +0300

    Uploading the eigth task from the FunctionalPrograming Exercise lesson

---
+++ Progaming C# Advance/05.FunctionalProgramming-Exercises/08.ListOfPredicates/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberMax = int.Parse(Console.ReadLine());
+            int[] dividers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            List<int> divisibleNumbers = new List<int>();
+            int lenghDividers = dividers.Length;
+
+            Func<int, int, bool> isDivisable = (x, y) => x % y == 0;
+            Predicate<int> isEnought = x => x < lenghDividers;
+
+            for (int i = 1; i <= numberMax; i++)
+            {
+                int j = 0;
+                bool isDivisible = true;
+                while (isEnought(j))
+                {
+                    if (isDivisable(i, dividers[j]))
+                    {
+                        isDivisible = true;
+                    }
+                    else
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+
+                    j++;
+                }
+
+                if (isDivisible) divisibleNumbers.Add(i);
+            }
+            Console.WriteLine(string.Join(" ", divisibleNumbers));
+
+        }
+    }
+}
+
+/*Alternative:
+ * 
+ * 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberMax = int.Parse(Console.ReadLine());
+            int[] dividers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            List<int> divisibleNumbers = new List<int>();
+            int lenghDividers = dividers.Length;
+
+            Func<int, int, bool> isDivisable = (x, y) => x % y == 0;
+            Predicate<int> isEnought = x => x < lenghDividers;
+
+            bool isDivisible = true;
+            for (int i = 1; i <= numberMax; i++)
+            {
+
+                foreach (var divider in dividers)
+                {
+
+                    if (!isDivisable(i, divider))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                  
+                }
+
+                if (isDivisible) divisibleNumbers.Add(i);
+                
+                isDivisible = true;
+
+            }
+            Console.WriteLine(string.Join(" ", divisibleNumbers));
+
+        }
+    }
+}
+
+ */
