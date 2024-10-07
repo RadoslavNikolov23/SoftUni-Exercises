4aae3305b1ef8a83c30b996e677f42f8d4c8381f
Rado <sitcom93@abv.bg>
2024-10-07 18:26:25 +0300

    Uploading the last task from the Generic Exercise lesson

---
+++ Progaming C# Advance/07.Generics-Exercises/09.CustomDoublyLinkedList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomDoublyLinkedList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList<int> myList = new DoublyLinkedList<int>();
+
+            for(int i = 0; i < 5; i++)
+            {
+                myList.AddFirst(i);
+            }
+
+            myList.RemoveLast();
+
+            int[] array = myList.ToArray();
+            myList.ForEach(x => Console.WriteLine(x));
+          
+        }
+    }
+}
