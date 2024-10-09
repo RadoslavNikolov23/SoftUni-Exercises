a2761f8c3d22fa776fce88747060610754138aad
Rado <sitcom93@abv.bg>
2024-10-09 13:44:33 +0300

    Uploading the task from the Iterators and Comparators lesson

---
+++ Progaming C# Advance/08.IteratorsAndComparators-Lab/01.Library/Library.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators;
+
+public class Library:IEnumerable<Book>
+{
+    public Library(params Book[] books)
+    {
+        this.books = new List<Book>(books);
+    }
+    private List<Book> books { get; set; }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
