7bac15c1e62eec58af630495bf997a9f4c4fc3d6
Rado <sitcom93@abv.bg>
2024-10-09 23:59:40 +0300

    Uploading the first two tasks from the Iterators and Comparators Exercise lesson

---
+++ Progaming C# Advance/08.IteratorsAndComparators-Exercise/01.ListyIterator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace IteratorsAndComparators;
+
+public class Program
+{
+    public static void Main()
+    {
+        string[] data = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        Debug.Assert(data[0] == "Create");
+
+        ListyIterator<string> listyList = new ListyIterator<string>(data[1..].ToList());
+
+        string commands = Console.ReadLine();
+        while (commands != "END")
+        {
+            try
+            {
+                switch (commands)
+                {
+                    case "Move":
+                        Console.WriteLine(listyList.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listyList.HasNext());
+                        break;
+                    case "Print":
+                        listyList.Print();
+                        break;
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            
+            
+            commands = Console.ReadLine();
+
+        }
+    }
+}
---
+++ Progaming C# Advance/08.IteratorsAndComparators-Exercise/02.Collection/ListyIterator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators;
+
+public class ListyIterator<T>:IEnumerable<T>
+{
+    public ListyIterator(List<T> element)
+    {
+        this.elements = element ?? throw new ArgumentNullException(nameof(element));
+        internalIndex = 0;
+    }
+
+    private List<T> elements;
+    private int internalIndex;
+
+    public bool Move()
+    {
+        if (!HasNext()) return false;
+
+        internalIndex++; 
+        return true;
+    }
+
+    public bool HasNext()
+    {
+        if(internalIndex+1<this.elements.Count) return true;
+        return false;
+    }
+
+    public void Print()
+    {
+        if(this.elements.Count!=0)
+        Console.WriteLine($"{this.elements[internalIndex]}");
+        else
+        Console.WriteLine("Invalid Operation!");
+    }
+
+    public void PrintAll()
+    {
+        for(int i = 0; i < this.elements.Count; i++)
+        {
+            if(i>0) Console.Write(" ");
+            Console.Write(elements[i]);
+        }
+        Console.WriteLine();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        foreach(T element in this.elements)
+        {
+            yield return element;
+        }
+
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()=>this.GetEnumerator();
+   
+}
