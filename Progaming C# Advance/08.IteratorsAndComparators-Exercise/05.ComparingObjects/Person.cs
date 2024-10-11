596d3c0f2bf8fa1fdd5340c678d48972570dd790
Rado <sitcom93@abv.bg>
2024-10-11 23:04:37 +0300

    Uploading the fifth task from the Iterators and Comparators Exercise Lesson

---
+++ Progaming C# Advance/08.IteratorsAndComparators-Exercise/05.ComparingObjects/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparingObjects
+{
+    public class Person:IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            Name = name;
+            Age = age;
+            Town = town;
+        }
+
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+
+        public int CompareTo(Person other)
+        {
+
+            
+            bool areEqualNames = this.Name.Equals(other.Name);
+            bool areEqualAge = this.Age.Equals(other.Age);
+            bool areEqualTowns = this.Town.Equals(other.Town);
+
+            if (areEqualNames && areEqualAge && areEqualTowns) return 0;
+            else return 1;
+           
+        }
+    }
+}
---
+++ Progaming C# Advance/08.IteratorsAndComparators-Exercise/05.ComparingObjects/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ComparingObjects;
+
+public class Program
+{
+    public static void Main()
+    {
+        string input=Console.ReadLine();
+        List<Person> personList= new List<Person>();
+
+        while (input != "END")
+        {
+            string[] inputArray=input.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+         Person person=new Person(inputArray[0], int.Parse(inputArray[1]), inputArray[2]);   
+
+            personList.Add(person);
+            input = Console.ReadLine();
+        }
+        int numberToCompare=int.Parse(Console.ReadLine());
+
+        Person personToCompare = personList[numberToCompare - 1];
+      
+
+
+        int countMathers = 0;
+        int numberNotEqual = 0;
+
+        foreach (Person person in personList)
+        { 
+            int result= person.CompareTo(personToCompare);
+
+            switch (result) {
+                case 1:
+                    numberNotEqual++;
+                    break;
+
+                case 0:
+                    countMathers++;
+                    break;
+
+            }
+        }
+
+        if (countMathers > 1)
+        Console.WriteLine($"{countMathers} {numberNotEqual} {personList.Count}");
+        else
+            Console.WriteLine("No matches");
+
+    }
+}
