6e8fc139845ff085d2ddb882f30dc9ccedc4926f
Rado <sitcom93@abv.bg>
2024-10-11 21:41:25 +0300

    Uploading the third task From the Iterators and Comparators Exercise lesson

---
+++ Progaming C# Advance/08.IteratorsAndComparators-Exercise/03.Stack/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Xml.Linq;
+
+namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = Console.ReadLine().Split(new char [] { ' ', ',' },StringSplitOptions.RemoveEmptyEntries);
+
+            Stack<int> stack = new Stack<int>(inputArray.Skip(1).Select(int.Parse).ToArray());
+
+            inputArray = Console.ReadLine().Split(new char[] { ' ', ',' });
+
+            while (inputArray[0] != "END")
+            {
+                switch (inputArray[0])
+                {
+                    case "Push":
+                        stack.Push(int.Parse(inputArray[1]));
+                        break;
+                    case "Pop":
+                        stack.Pop();
+                        break;
+                }
+
+                inputArray = Console.ReadLine().Split(new char[] { ' ', ',' });
+            }
+
+
+            foreach (int element in stack)
+            {
+                Console.WriteLine(element);
+            }  
+            foreach (int element in stack)
+            {
+                Console.WriteLine(element);
+            }
+        
+        }
+
+     
+    }
+}
---
+++ Progaming C# Advance/08.IteratorsAndComparators-Exercise/03.Stack/Stack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Stack<T>:IEnumerable<T>
+    {
+        public Stack(T[] elements)
+        {
+            this.CustomST = new List<T>(elements);
+        }
+        public List<T> CustomST { get; set; }
+        
+        public void Push(T[] elements)
+        {
+            foreach(T element in elements)
+            this.CustomST.Add(element);
+        }   
+        public void Push(T element)
+        {
+            this.CustomST.Add(element);
+        }   
+        
+        public T Pop()
+        {
+            if(this.CustomST.Count == 0)
+            {
+                    Console.WriteLine("No elements");
+                return default(T) ;
+
+            }
+
+            T element=CustomST[CustomST.Count-1];
+             this.CustomST.RemoveAt(this.CustomST.Count-1);
+             return element;
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for(int i=this.CustomST.Count-1; i >= 0; i--)
+            {
+                yield return this.CustomST[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+     
+
+    }
+}
