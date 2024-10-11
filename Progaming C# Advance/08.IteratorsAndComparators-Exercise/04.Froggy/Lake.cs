ada53ad27ecf382941f1bd148a7700c63f67d178
Rado <sitcom93@abv.bg>
2024-10-11 22:10:11 +0300

    Uploading the forth task from the Iterators and Comparators Exercise lesson

---
+++ Progaming C# Advance/08.IteratorsAndComparators-Exercise/04.Froggy/Lake.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Froggy
+{
+    public class Lake:IEnumerable<int>
+    {
+        public Lake()
+        {
+            this.MyList = new List<int>();
+        }
+        public Lake(List<int> myList):this()
+        {
+            this.MyList = myList;
+        }
+        public List<int> MyList { get; }
+        public IEnumerator<int> GetEnumerator()
+        {
+            foreach(int number in this.MyList)
+            {
+                yield return number;
+            }
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        
+    }
+}
---
+++ Progaming C# Advance/08.IteratorsAndComparators-Exercise/04.Froggy/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Froggy;
+
+public class Program
+{
+    static void Main()
+    {
+        Lake numbers = new Lake(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse).ToList());
+
+        List<int> finalNumbers = new List<int>();
+
+        for (int i = 0; i < numbers.Count(); i += 2)
+        {
+            finalNumbers.Add(numbers.MyList[i]);
+        }
+
+        Func<int, int> FindReverStart=num=>{ if (num % 2 == 0) return num - 1; else return num - 2; };
+
+        for (int i = FindReverStart(numbers.Count()); i >= 0; i -= 2)
+        {
+            finalNumbers.Add(numbers.MyList[i]);
+        }
+
+        Console.WriteLine(string.Join(", ", finalNumbers));
+
+
+
+    }
+}
