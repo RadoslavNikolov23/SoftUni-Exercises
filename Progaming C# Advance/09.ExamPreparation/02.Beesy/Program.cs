a3110e1507b4d60920198d8119df94877ad05a92
Rado <sitcom93@abv.bg>
2024-10-12 15:59:43 +0300

    Uploading the rest of the tasks from the Exam Preparation lesson

---
+++ Progaming C# Advance/09.ExamPreparation/02.Beesy/Program.cs
@@ -0,0 +1,119 @@
+﻿namespace _02.Beesy;
+
+public class Program
+{
+    public static void Main()
+    {
+        int numberRowCol = int.Parse(Console.ReadLine());
+
+        char[,] matrix = new char[numberRowCol, numberRowCol];
+
+        int rowB = 0, colB = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            string data = Console.ReadLine();
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = data[j];
+
+                if (matrix[i, j] == 'B')
+                {
+                    rowB = i;
+                    colB = j;
+                }
+            }
+        }
+
+        int unitEnergy = 15;
+        int collectNectar = 0;
+
+        bool reachTheHive = false;
+        bool isRestoredAlready = true;
+
+        while (true)
+        {
+            string command = Console.ReadLine();
+            unitEnergy--;
+
+            matrix[rowB, colB] = '-';
+            switch (command)
+            {
+                case "up":
+                    if (rowB - 1 >= 0) rowB--;
+                    else rowB = matrix.GetLength(0) - 1;
+                    break;
+
+                case "down":
+                    if (rowB + 1 > matrix.GetLength(0) - 1) rowB = 0;
+                    else rowB++;
+                    break;
+
+                case "left":
+                    if (colB - 1 >= 0) colB--;
+                    else colB = matrix.GetLength(1) - 1;
+                    break;
+
+                case "right":
+                    if (colB + 1 > matrix.GetLength(1) - 1) colB = 0;
+                    else colB++;
+                    break;
+            }
+           
+
+            if (char.IsDigit(matrix[rowB, colB]))
+            {
+                collectNectar += int.Parse(matrix[rowB, colB].ToString());
+                matrix[rowB, colB] = '-';
+            }
+
+            if (matrix[rowB, colB] == 'H')
+            {
+                reachTheHive = true;
+                break;
+            }
+
+            if (unitEnergy <= 0)
+            {
+                if (collectNectar < 30) break;
+                else
+                {
+                    if (isRestoredAlready)
+                    {
+                        unitEnergy = collectNectar - 30;
+                        isRestoredAlready = false;                      
+                    }
+                    else break;
+                }
+            }
+        }
+        matrix[rowB, colB] = 'B';
+
+
+        if(reachTheHive && collectNectar >= 30)
+        {
+            Console.WriteLine($"Great job, Beesy! The hive is full. Energy left: {unitEnergy}");
+        }
+        else if (reachTheHive && collectNectar<30)
+        {
+            Console.WriteLine($"Beesy did not manage to collect enough nectar.");
+        }
+        else
+        {
+            Console.WriteLine($"This is the end! Beesy ran out of energy.");
+        }
+
+        PrintMatrix(matrix);
+    }
+
+    public static void PrintMatrix<T>(T[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Progaming C# Advance/09.ExamPreparation/03.EstateAgency/EstateAgency.cs
@@ -0,0 +1,73 @@
+﻿using System.Net;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace EstateAgency
+{
+    public class EstateAgency
+    {
+        public EstateAgency(int capacity)
+        {
+            Capacity = capacity;
+            RealEstates=new List<RealEstate> ();
+        }
+        public List<RealEstate> RealEstates { get; set; }
+        public int Capacity { get; set; }
+        public int Count
+        {
+            get
+            { return RealEstates.Count; }
+        }
+
+        public void AddRealEstate(RealEstate realEstate)
+        {
+            if (Capacity > Count)
+            {
+                if(!RealEstates.Any(x=>x.Address== realEstate.Address))
+                         RealEstates.Add(realEstate);
+            }
+        }
+
+        public bool RemoveRealEstate(string address)
+        {
+            int indexAt=RealEstates.FindIndex(x=>x.Address== address);
+
+            if (indexAt == -1) return false;
+
+            RealEstates.RemoveAt(indexAt);
+            return true;
+
+        }
+
+        public List<RealEstate> GetRealEstates(string postalCode)
+        {
+            return RealEstates.Where(x => x.PostalCode == postalCode).ToList();
+        }
+
+        public RealEstate GetCheapest()
+        {
+            RealEstate cheapestRealEstate = RealEstates.OrderBy(x=>x.Price).First();
+            return cheapestRealEstate;
+        }
+
+        public double GetLargest()
+        {
+            RealEstate largestRealEstate= RealEstates.OrderByDescending(x => x.Size).First();
+            return largestRealEstate.Size;
+        }
+
+        public string EstateReport()
+        {
+            StringBuilder sbResult= new StringBuilder();
+            sbResult.AppendLine("Real estates available:");
+            foreach(var realEstate in RealEstates)
+            {
+                sbResult.AppendLine(realEstate.ToString());
+            }
+
+            return sbResult.ToString().Trim();
+        }
+
+ 
+    }
+}
