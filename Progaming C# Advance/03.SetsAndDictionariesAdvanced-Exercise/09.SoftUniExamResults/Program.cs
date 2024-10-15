66588ac852830195c438157f42ddb6dbbcae0b7f
Rado <156640386+RadoslavNikolov23@users.noreply.github.com>
2024-10-15 16:19:45 +0300

    Uploading the last tasks from the Sets and Dictionaries Exercise lesson

---
+++ Progaming C# Advance/03.SetsAndDictionariesAdvanced-Exercise/09.SoftUniExamResults/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.SoftUniExamResults
+{
+    public class Student
+    {
+        public Student(string username, string language, int points)
+        {
+            Username = username;
+            Language = language;
+            Points = points;
+        }
+
+        public string Username { get; set; }
+        public string Language { get; set; }
+        public int Points { get; set; }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, List<int>> submissionDT = new Dictionary<string, List<int>>();
+            Dictionary<string, Student> studentDT  = new Dictionary<string, Student>();
+
+            while(input!= "exam finished")
+            {
+                string[] array = input.Split("-",StringSplitOptions.RemoveEmptyEntries);
+             
+                string username =string.Empty;
+                string language = string.Empty;
+                int points = 0;
+                if (array.Length >= 3)
+                {
+                    username = array[0];
+                    language = array[1];
+                    points = int.Parse(array[2]);
+
+                    Student student = new Student(username, language, points);
+                    if (!studentDT.ContainsKey(username)) studentDT[username] = student;
+                    else if (studentDT[username].Points < student.Points) studentDT[username].Points = points;
+                    if (!submissionDT.ContainsKey(language)) submissionDT[language] = new List<int>() { 1 };
+                    else submissionDT[language].Add(1);
+                }
+                else
+                {
+                    username = array[0];
+                    studentDT.Remove(username);
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Results:");
+            foreach(var student in studentDT.OrderByDescending(x=>x.Value.Points).ThenBy(x=>x.Key))
+            {
+                Console.WriteLine($"{student.Key} | {student.Value.Points}");
+            }
+            Console.WriteLine("Submissions:");
+            foreach(var student in submissionDT.OrderByDescending(x=>x.Value.Count).ThenBy(x=>x.Key))
+            {
+                Console.WriteLine($"{student.Key} - {student.Value.Count}");
+            }
+
+        }
+    }
+}
---
+++ Progaming C# Advance/03.SetsAndDictionariesAdvanced-Exercise/10.ForceBook/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.ForceBook
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+
+            Dictionary<string, List<string>> forceDT = new Dictionary<string, List<string>>();
+            while (input != "Lumpawaroo")
+            {
+
+                if (input.Contains('|'))
+                {
+                    string[] data = input.Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+                    string forceSide = data[0];
+                    string forceUser = data[1];
+
+                    if (!forceDT.ContainsKey(forceSide))
+                    {
+                        forceDT.Add(forceSide, new List<string>());
+                    }
+
+                    if (!forceDT.Any(x => x.Value.Contains(forceUser)))
+                    {
+                        forceDT[forceSide].Add(forceUser);
+                    }
+
+
+                }
+                else
+                {
+                    string[] data = input.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                    string forceUser = data[0];
+                    string forceSide = data[1];
+
+
+                    if (!forceDT.ContainsKey(forceSide))
+                    {
+                        forceDT.Add(forceSide, new List<string>());
+
+                    }
+
+                    if (forceDT.Any(x => x.Value.Any(i => i == forceUser)))
+                    {
+                        var origSide = GetKey(forceDT, forceUser);
+
+                        forceDT[origSide].Remove(forceUser);
+                        forceDT[forceSide].Add(forceUser);
+                        Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+
+                    }
+                    else
+                    {
+                        forceDT[forceSide].Add(forceUser);
+                        Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+                    }
+
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var forceSide in forceDT.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+            {
+                if (forceSide.Value.Count != 0)
+                {
+                    Console.WriteLine($"Side: {forceSide.Key}, Members: {forceSide.Value.Count}");
+
+                    Console.WriteLine($"! {string.Join("\n! ", forceSide.Value.OrderBy(x => x))}");
+                }
+
+
+            }
+
+        }
+
+
+
+        public static string GetKey(Dictionary<string, List<string>> dictionary, string forseUser)
+        {
+            foreach (var keyValue in dictionary)
+            {
+                foreach (var value in keyValue.Value)
+                {
+                    if (value == forseUser)
+                    {
+                        return keyValue.Key;
+                    }
+                }
+
+            }
+            return null;
+        }
+    }
+}
