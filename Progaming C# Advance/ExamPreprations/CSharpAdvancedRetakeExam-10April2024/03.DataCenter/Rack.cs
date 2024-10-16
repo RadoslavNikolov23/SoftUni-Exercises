871796cf9b7536bf073565a85fdeee2a0ffc5e1d
Rado <sitcom93@abv.bg>
2024-10-16 16:49:32 +0300

    Uploading the last task from a old Exam Preparation

---
+++ Progaming C# Advance/ExamPreprations/CSharpAdvancedRetakeExam-10April2024/03.DataCenter/Rack.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataCenter
+{
+    public class Rack
+    {
+        public Rack(int slots)
+        {
+            this.Slots = slots;
+            this.Servers = new List<Server>();
+        }
+        public int Slots { get; set; }
+        public List<Server> Servers { get; set; }
+        public int GetCount { get { return Servers.Count; } }
+
+
+        public void AddServer(Server server)
+        {
+            if (GetCount >= Slots) return;
+
+            if (!this.Servers.Any(x => x.SerialNumber == server.SerialNumber)) Servers.Add(server);
+
+        }
+
+        public  bool RemoveServer(string serialNumber)
+        {
+            Server server = this.Servers.FirstOrDefault(x => x.SerialNumber == serialNumber);
+
+            return Servers.Remove(server);
+        }
+
+        public string GetHighestPowerUsage()
+        {
+            Server server = this.Servers.OrderByDescending(x=>x.PowerUsage).First();
+            return server.ToString();
+        }
+
+        public int GetTotalCapacity()
+        {
+            return Servers.Sum(x => x.Capacity);  //TODO??
+        }
+
+        public string DeviceManager()
+        {
+            StringBuilder sbResult = new StringBuilder();
+            sbResult.AppendLine($"{this.GetCount} servers operating:");
+            sbResult.AppendLine($"{string.Join("\n",Servers)}");
+
+            return sbResult.ToString().Trim();
+        }
+
+    }
+}
---
+++ Progaming C# Advance/ExamPreprations/CSharpAdvancedRetakeExam-10April2024/03.DataCenter/Server.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataCenter
+{
+    public class Server
+    {
+        public Server(string serialNumber, string model, int capacity, int powerUsage)
+        {
+            SerialNumber = serialNumber;
+            Model = model;
+            Capacity = capacity;
+            PowerUsage = powerUsage;
+        }
+
+        public string SerialNumber { get; set; }
+        public string Model { get; set; }
+        public int Capacity { get; set; }
+        public int PowerUsage { get; set; }
+
+        public override string ToString()
+        {
+             StringBuilder sbResult = new StringBuilder();
+            sbResult.AppendLine($"Server {this.SerialNumber}: {this.Model}, {this.Capacity}TB, {this.PowerUsage}W");
+
+            return sbResult.ToString().Trim();
+        }
+    }
+}
