0ff161ea342356e3d4eae271d5872b0f00c8ed2b
Rado <sitcom93@abv.bg>
2024-10-18 23:22:41 +0300

    Uploading some extra ExamPreparataion tasks

---
+++ Progaming C# Advance/ExamPreprations/CSharpAdvancedRetakeExam-17February2024/01.ChickenSnack/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _01.ChickenSnack;
+
+public class Program
+{
+    public static void Main()
+    {
+        Stack<int> amountOfMoney = new Stack<int>(Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+        Queue<int> priceOfFood = new Queue<int>(Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+        int countEatenFood = 0;
+
+        while (amountOfMoney.Count > 0 && priceOfFood.Count > 0)
+        {
+            int lastMoney = amountOfMoney.Pop();
+            int firstFood = priceOfFood.Dequeue();
+
+            if (lastMoney > firstFood)
+            {
+                int change = lastMoney - firstFood;
+
+                if (amountOfMoney.Count == 0) amountOfMoney.Push(change);
+                else amountOfMoney.Push(amountOfMoney.Pop() + change);
+                countEatenFood++;
+
+            }
+            else if ( lastMoney == firstFood) countEatenFood++;
+
+        }
+      
+
+        if (countEatenFood == 0) Console.WriteLine($"Henry remained hungry. He will try next weekend again.");
+        else if (countEatenFood >= 4) Console.WriteLine($"Gluttony of the day! Henry ate {countEatenFood} foods.");
+        else if (countEatenFood == 1) Console.WriteLine($"Henry ate: {countEatenFood} food.");
+        else Console.WriteLine($"Henry ate: {countEatenFood} foods.");
+
+
+    }
+}
