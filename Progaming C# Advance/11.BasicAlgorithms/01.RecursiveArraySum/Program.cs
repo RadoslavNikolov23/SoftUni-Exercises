44df069c12693ab331dc454ca133b9144a7a71b6
Rado <sitcom93@abv.bg>
2024-10-19 17:26:38 +0300

    Uploading some tasks from the Basic Algorithms - Exercise

---
+++ Progaming C# Advance/11.BasicAlgorithms/01.RecursiveArraySum/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _01.RecursiveArraySum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array=Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int sumAll = SumAllElemets(array);
+            Console.WriteLine(sumAll);
+        }
+
+        private static int SumAllElemets(int[] array, int index=0, int sum=0)
+        {
+           
+            if (index >= array.Length) return 0;
+
+            sum += array[index]+SumAllElemets(array,index+1);
+            return sum;
+        }
+    }
+}
---
+++ Progaming C# Advance/11.BasicAlgorithms/02.RecursiveFactorial/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _02.RecursiveFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number=int.Parse(Console.ReadLine());
+
+            Console.WriteLine(FactorialOf(number));
+        }
+
+        private static int FactorialOf(int number)
+        {
+            if (number == 1) return 1;
+
+            number *= FactorialOf(number - 1);
+            return number;
+        }
+    }
+}
