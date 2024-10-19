09c9e672e5713a3ca68a38fab7ff58d3c342b238
Rado <sitcom93@abv.bg>
2024-10-19 10:03:03 +0300

    Uploading the last task from the ExamPreparation from February 2024

---
+++ Progaming C# Advance/ExamPreprations/CSharpAdvancedRetakeExam-17February2024/03.SharkTaxonomy/SharkTaxonomy/Classifier.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace SharkTaxonomy
+{
+    public class Classifier
+    {
+        public Classifier(int capacity)
+        {
+            this.Capacity = capacity;
+            this.Species = new List<Shark>();
+        }
+
+        public int Capacity { get; set; }
+        public List<Shark> Species { get; set; }
+        public int GetCount
+        {
+            get { return Species.Count; }
+       
+        }
+
+
+        public void AddShark(Shark shark)
+        {
+            if (this.Capacity <= GetCount) return;
+
+            if(!this.Species.Any(x=>x.Kind==shark.Kind)) Species.Add(shark);
+        }
+
+
+        public bool RemoveShark(string kind)
+        {
+            Shark shark=this.Species.FirstOrDefault(x=>x.Kind==kind);
+
+            return Species.Remove(shark);
+        }
+
+        public string GetLargestShark() => this.Species.OrderByDescending(x => x.Length).First().ToString().Trim();
+
+        public double GetAverageLength() => this.Species.Average(x=>x.Length);
+
+
+        public string Report()
+        {
+            StringBuilder sharkSB= new StringBuilder();
+            sharkSB.AppendLine($"{this.GetCount} sharks classified:");
+            sharkSB.AppendLine($"{string.Join("\n",this.Species)}");
+            sharkSB.AppendLine($"");
+
+            return sharkSB.ToString().Trim();
+        }
+ 
+    }
+}
---
+++ Progaming C# Advance/ExamPreprations/CSharpAdvancedRetakeExam-17February2024/03.SharkTaxonomy/SharkTaxonomy/Shark.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace SharkTaxonomy
+{
+    public class Shark
+    {
+        public Shark(string kind, int length, string food, string habitat)
+        {
+            Kind = kind;
+            Length = length;
+            Food = food;
+            Habitat = habitat;
+        }
+
+        public string Kind { get; set; }
+        public int Length { get; set; }
+        public string Food { get; set; }
+        public string Habitat { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sbResult = new StringBuilder();
+            sbResult.AppendLine($"{this.Kind} shark: {this.Length}m long.");
+            sbResult.AppendLine($"Could be spotted in the {this.Habitat}, typical menu: {this.Food}");
+
+            return sbResult.ToString().Trim();
+        }
+
+    }
+}
