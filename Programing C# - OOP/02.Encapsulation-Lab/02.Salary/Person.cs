3b0ec366190d30cfdd96ee5838af78a6d1a879e2
Rado <sitcom93@abv.bg>
2024-10-26 11:07:50 +0300

    Uploading all the tasks from the Encapsulation lesson

---
+++ Programing C# - OOP/02.Encapsulation-Lab/02.Salary/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+
+        public string FirstName
+        {
+            get { return firstName; }
+            private set { firstName = value; }
+        }
+
+
+        public string LastName
+        {
+            get { return lastName; }
+           private set { lastName = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            private set { age = value; }
+        }
+
+
+        public decimal Salary
+        {
+            get { return salary; }
+            private set { salary = value; }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            decimal increaseSalary=this.Salary*percentage/100;
+            if (this.Age < 30) increaseSalary /= 2;
+
+            this.Salary += increaseSalary;
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+        }
+    }
+}
---
+++ Programing C# - OOP/02.Encapsulation-Lab/04.Team/Team.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        public Team(string name)
+        {
+                this.Name= name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+        private string name;
+        private List<Person> firstTeam;
+
+		private List<Person> reserveTeam;
+
+
+		public string Name
+		{
+			get { return name; }
+			private set { name = value; }
+		}
+
+
+		public IReadOnlyCollection<Person> FirstTeam
+		{
+			get { return this.firstTeam.AsReadOnly(); }
+		}
+
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get { return this.reserveTeam.AsReadOnly(); }
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if(person.Age<40)
+            this.firstTeam.Add(person);
+            else
+                this.reserveTeam.Add(person);
+        }
+
+        public override string ToString()
+        {
+            return $"First team has {FirstTeam.Count} players.{Environment.NewLine}Reserve team has {ReserveTeam.Count} players.";
+
+        }
+    }
+}
