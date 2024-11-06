1ccafd453544011dd89bda5a7e9ae9afe2ab484c
Rado <sitcom93@abv.bg>
2024-11-06 23:39:03 +0200

    Uploading some the first task from the Exception and Error Handling Lab

---
+++ Programing C# - OOP/05.ExceptionsAndErrorHandling-Lab/01.SquareRoot/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _01.SquareRoot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                double squareRoot = Math.Sqrt(number);
+                double nan = double.NaN;
+
+                if (double.IsNaN(squareRoot))
+                    throw new ArgumentException("Invalid number.");
+
+                 Console.WriteLine(squareRoot);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye");
+            }
+
+
+        }
+    }
+}
