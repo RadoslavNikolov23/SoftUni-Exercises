5ec4b100d025bd58d72c6f8b9c8169486e2715f8
Rado <sitcom93@abv.bg>
2024-11-09 20:33:32 +0200

    Uploading the fifth task from the Exception and Error Handling lesson

---
+++ Programing C# - OOP/05.ExceptionsAndErrorHandling-Lab/04.SumOfIntegers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Xml.Linq;
+
+namespace _04.SumOfIntegers
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string[] diffrentTypes = Console.ReadLine().Split(" ");
+
+            int sumAllValidNumbers = 0;
+
+            for (int i = 0; i < diffrentTypes.Length; i++)
+            {
+                try
+                {
+                    int number = int.Parse(diffrentTypes[i]);
+                    sumAllValidNumbers += number;
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine($"The element '{diffrentTypes[i]}' is in wrong format!");
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine($"The element '{diffrentTypes[i]}' is out of range!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{diffrentTypes[i]}' processed - current sum: {sumAllValidNumbers}");
+                }
+            }
+            Console.WriteLine($"The total sum of all integers is: {sumAllValidNumbers}");
+
+        }
+    }
+}
