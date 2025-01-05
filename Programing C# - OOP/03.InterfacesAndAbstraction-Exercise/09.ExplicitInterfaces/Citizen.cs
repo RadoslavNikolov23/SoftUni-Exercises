10176ae171ef47a6706dcda933edafb0e5a2b9a0
Rado <sitcom93@abv.bg>
2025-01-05 14:24:02 +0200

    Uploading the last task from the Interfaces and Abstraction Exercise lesson

---
+++ Programing C# - OOP/03.InterfacesAndAbstraction-Exercise/09.ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.ExplicitInterfaces
+{
+    public class Citizen : IPerson, IResident
+    {
+        public Citizen(string name, string country, int age )
+        {
+            Name = name;
+            Country = country;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Age { get; set; }
+
+        public string GetName()
+        {
+            return ((IPerson)this).Name;
+        }
+
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {((IPerson)this).Name}";
+        }
+    }
+}
---
+++ Programing C# - OOP/03.InterfacesAndAbstraction-Exercise/09.ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace _09.ExplicitInterfaces
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] citizenInfo = input.Split();
+                string name = citizenInfo[0];
+                string country = citizenInfo[1];
+                int age = int.Parse(citizenInfo[2]);
+                Citizen citizen = new Citizen(name, country, age);
+                Console.WriteLine(((IPerson)citizen).GetName());
+                Console.WriteLine(((IResident)citizen).GetName());
+            }
+        }
+    }
+}
