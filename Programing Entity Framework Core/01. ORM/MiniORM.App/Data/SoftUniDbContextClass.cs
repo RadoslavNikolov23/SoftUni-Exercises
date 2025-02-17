3b2f6c35685b5aa8c0bb210d08d82e6f9c81e041
Radoslav Nikolov <156640386+RadoslavNikolov23@users.noreply.github.com>
2025-02-17 16:07:01 +0200

    Uploading the MiniORM app console

---
+++ Programing Entity Framework Core/01. ORM/MiniORM.App/Data/SoftUniDbContextClass.cs
@@ -0,0 +1,20 @@
+﻿using MiniORM.App.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniORM.App.Data
+{
+    public class SoftUniDbContextClass : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<EmployeeProject> EmployeeProjects { get; set; }
+        public SoftUniDbContextClass(string connectionString) : base(connectionString)
+        {
+        }
+    }
+}
---
+++ Programing Entity Framework Core/01. ORM/MiniORM.App/Program.cs
@@ -0,0 +1,32 @@
+﻿using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+
+namespace MiniORM.App
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string connectionString = "Server=.;Database=MiniORM;Integrated Security=True;Encrypt=False";
+
+            SoftUniDbContextClass context = new SoftUniDbContextClass(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DeparmentId = context.Departments.First().Id,
+                IsEmployed =true
+            });
+
+            Employee employee = context.Employees.Last();
+            employee.FirstName = "Modifiel";
+
+
+            Console.WriteLine($"{employee.FirstName} {employee.LastName} - {employee.DeparmentId} {employee.IsEmployed}");
+
+            context.SaveChanges();
+
+        }
+    }
+}
