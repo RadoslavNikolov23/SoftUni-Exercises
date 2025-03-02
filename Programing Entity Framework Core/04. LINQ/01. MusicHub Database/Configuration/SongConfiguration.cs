8f270d51eb3f1a796cc925acd38306ce08dcc53c
Rado <sitcom93@abv.bg>
2025-03-02 12:18:42 +0200

    Uploading the first task from the LIQN Exercises

---
+++ Programing Entity Framework Core/04. LINQ/01. MusicHub Database/Configuration/SongConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MusicHub.Data.Models;
+using static MusicHub.Validators.ModelValidators;
+
+namespace MusicHub.Configuration
+{
+    public class SongConfiguration : IEntityTypeConfiguration<Song>
+    {
+        public void Configure(EntityTypeBuilder<Song> entity)
+        {
+            entity
+                .HasKey(e => e.Id);
+
+            entity
+                .Property(e => e.Name)
+                .IsRequired()
+                .IsUnicode()
+                .HasMaxLength(SongValidator.songNameLength);
+
+            entity
+                .Property(e => e.Duration)
+                .IsRequired();
+
+            entity
+                .Property(e => e.CreatedOn)
+                .IsRequired();
+
+            entity
+                .Property(e => e.Genre)
+                .IsRequired();
+
+            entity
+                .Property(e => e.Price)
+                .IsRequired();
+
+            entity
+                .HasOne(e => e.Album)
+                .WithMany(a => a.Songs)
+                .HasForeignKey(e => e.AlbumId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entity
+                .HasOne(e => e.Writer)
+                .WithMany(w => w.Songs)
+                .HasForeignKey(e => e.WriterId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
---
+++ Programing Entity Framework Core/04. LINQ/01. MusicHub Database/Configuration/WriterConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MusicHub.Data.Models;
+using static MusicHub.Validators.ModelValidators;
+
+
+namespace MusicHub.Configuration
+{
+    public class WriterConfiguration : IEntityTypeConfiguration<Writer>
+    {
+        public void Configure(EntityTypeBuilder<Writer> entity)
+        {
+            entity
+                .HasKey(e => e.Id);
+
+            entity
+                .Property(e => e.Name)
+                .IsRequired()
+                .IsUnicode()
+                .HasMaxLength(WriterValidator.writerNameLength);
+
+            entity
+                .Property(e => e.Pseudonym)
+                .IsRequired(false)
+                .HasMaxLength(WriterValidator.writerPseudonymLength);
+
+        }
+    }
+}
