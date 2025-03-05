6d0e29b182582e9d96f2a92b2e0bd2123539215b
Rado <sitcom93@abv.bg>
2025-03-05 18:30:08 +0200

    Uploading the second task from the Advance Querying

---
+++ Programing Entity Framework Core/05. Advanced Querying/02. Age Restriction/BookShop/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine();
+            string result = GetBooksByAgeRestriction(db, input);
+            Console.WriteLine(result);
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+
+            bool areEquial = Enum.TryParse(command, true, out AgeRestriction ageCommand);
+
+
+            if (string.IsNullOrEmpty(command) || !areEquial)
+            {
+                return default;
+            }
+
+            var books = context.Books
+                .Where(b => b.AgeRestriction==ageCommand)
+                .Select(b => b.Title)
+                .OrderBy(b=>b)
+                .ToArray();
+
+            //StringBuilder sb = new StringBuilder();
+
+            //foreach(var book in books)
+            //    sb.AppendLine(book.Title)
+
+
+            return string.Join(Environment.NewLine, books);
+        }
+    }
+}
+
+
