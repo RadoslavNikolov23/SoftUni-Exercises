0006f0e6c543ca18a4a51359e7ab2d90d55d788e
Rado <sitcom93@abv.bg>
2025-05-15 20:48:48 +0300

    Updating

---
+++ Fundamentals/02.DataTypesAndVariables-MoreExercis/03.FloatingEquality/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Numerics;
+
+namespace _03.FloatingEquality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // string[] numbers = Console.ReadLine().Split();
+            double leftNumber = double.Parse(Console.ReadLine());
+            double rightNumber = double.Parse(Console.ReadLine());
+            double eps = 0.000001;
+            double diffrence;
+
+            if (leftNumber > rightNumber)
+            {
+                diffrence = leftNumber - rightNumber;
+            }
+            else
+            {
+                diffrence = rightNumber - leftNumber;
+            }
+
+            if (eps > diffrence)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+        }
+    }
+}
