ff7a0474517a6b0d741e746cdd9042f9ad90d0bd
Radoslav Nikolov <156640386+RadoslavNikolov23@users.noreply.github.com>
2025-06-18 12:09:24 +0300

    Create DestinationFavoriteViewModel.cs

---
+++ ASP.NET Fundamentals/01.Exam Preparation/Horizons.Web.ViewModels/Destinations/DestinationFavoriteViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Horizons.Web.ViewModels.Destinations
+{
+    using System.ComponentModel.DataAnnotations;
+    using static Horizons.GCommon.ValidationConstants.TerrainConst;
+   
+    public class DestinationFavoriteViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(NameMinLength)]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        public string Terrain { get; set} = null!;
+
+        public string? ImageUrl  {get; set:}
+    }
+}
