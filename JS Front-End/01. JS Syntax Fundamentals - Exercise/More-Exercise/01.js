9ef4e780721695bad2101e2e91837dc2cb59f630
Radoslav Nikolov <156640386+RadoslavNikolov23@users.noreply.github.com>
2025-11-04 00:25:16 +0200

    Upload the first task from the More-Exercise

---
+++ JS Front-End/01. JS Syntax Fundamentals - Exercise/More-Exercise/01.js
@@ -0,0 +1,34 @@
+function solve(numOneX, numOneY, numTwoX, numTwoY) {
+
+    let result = Math.sqrt(Math.pow((numOneX - 0),2) + Math.pow((numOneY - 0),2));
+
+    if(result % 1 === 0){
+        console.log(`{${numOneX}, ${numOneY}} to {${0}, ${0}} is valid`);
+    }
+    else{
+        console.log(`{${numOneX}, ${numOneY}} to {${0}, ${0}} is invalid`);
+    }
+
+    result = Math.sqrt(Math.pow((numTwoX - 0),2) + Math.pow((numTwoY - 0),2));
+
+    if(result % 1 === 0){
+        console.log(`{${numTwoX}, ${numTwoY}} to {${0}, ${0}} is valid`);
+    }
+    else{
+        console.log(`{${numTwoX}, ${numTwoY}} to {${0}, ${0}} is invalid`);
+    }
+
+    result = Math.sqrt(Math.pow((numTwoX - numOneX),2) + Math.pow((numTwoY - numOneY),2));
+
+    if(result % 1 === 0){
+        console.log(`{${numOneX}, ${numTwoX}} to {${numOneY}, ${numTwoY}} is valid`);
+    }
+    else{
+        console.log(`{${numOneX}, ${numTwoX}} to {${numOneY}, ${numTwoY}} is invalid`);
+    }
+
+}
+
+
+solve(3, 0, 0, 4);
+solve(2, 1, 1, 1);
